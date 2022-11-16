d616050a166df08e2b2c8ceeb5cfa1ddf5cb571d
pabijuta <baibucio@inbox.lv>
2022-11-16 22:03:11 +0200

    Tests GITam

--- src/lv/javaguru/demo/lecture5/bumbinas.java
+++ src/lv/javaguru/demo/lecture5/bumbinas.java
@@ -1,18 +1,27 @@
 package lv.javaguru.demo.lecture5;
 
+import java.util.Random;
+
 import java.util.Scanner;
 
 public class bumbinas {
 
     public static void main(String[] args) {
+        Random random = new Random();
+        int random_number;
+        random_number = random.nextInt(3) + 1;
+
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Izvēlies krūzīti no 1 līdz 3");
+        System.out.println("Lūdzu izvēlies krūzīti no 1 līdz 3!");
 
         int number;
         do {
             number = scanner.nextInt();
-            System.out.println("Tu izvēlējies krūzīti Nr.: " + number);
-        } while (number != 3);
+            if (number == random_number) {
+                System.out.println("Tu izvēlējies " + random_number);
+            } else {
+                System.out.println("Tu izvēlējies " + number + " un neuzminēji!");
+            }
+        } while (number != random_number);
     }
-
 }
