27524be7b9463d2f33f5db7a76fb0c244271d199
pabijuta <baibucio@inbox.lv>
2022-11-16 21:47:16 +0200

    Tests GITam

--- src/lv/javaguru/demo/lecture5/bumbinas.java
+++ src/lv/javaguru/demo/lecture5/bumbinas.java
@@ -1,6 +1,18 @@
 package lv.javaguru.demo.lecture5;
 
+import java.util.Scanner;
+
 public class bumbinas {
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Izvēlies krūzīti no 1 līdz 3");
+
+        int number;
+        do {
+            number = scanner.nextInt();
+            System.out.println("Tu izvēlējies krūzīti Nr.: " + number);
+        } while (number != 3);
+    }
 
 }
