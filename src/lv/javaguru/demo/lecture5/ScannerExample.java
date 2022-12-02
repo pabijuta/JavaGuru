59c2b63f236ce8467df591a0abb648fa05277c10
pabijuta <baibucio@inbox.lv>
2022-12-02 21:43:31 +0200

    Tests GITam

--- src/lv/javaguru/demo/lecture5/ScannerExample.java
+++ src/lv/javaguru/demo/lecture5/ScannerExample.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class ScannerExample {
 
     public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Lūdzu ievadi skaitli");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lūdzu ievadi skaitli");
 
-    int number;
-    do {
-        number = scanner.nextInt();
-        System.out.println("Jūs ievadījāt:" + number);
-    } while (number != -1);
+        int number;
+        do {
+            number = scanner.nextInt();
+            System.out.println("Jūs ievadījāt:" + number);
+        } while (number != -1);
     }
-    }
+}
 
